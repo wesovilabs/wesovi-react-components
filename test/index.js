dd21549dc01a9ec006bcf154dd1ee3da9c1e9ed1
Iván Corrales <developer@wesovi.com>
2016-06-11 06:59:20 +0200

    feat: working around dependencies

---
+++ test/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Ivan on 11/6/16.
+ */
--- webpack.config.development.js
+++ webpack.config.development.js
@@ -5,41 +5,52 @@ const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    context: __dirname,
-    devtool: 'inline-source-map',
-    entry: [
-        'webpack-hot-middleware/client',
-        './spec/index.js'
+  context: __dirname,
+  devtool: 'inline-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './test/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'spec.js',
+    publicPath: '/build/'
+  },
+  resolve: {
+    extensions: ['', '.scss', '.js', '.json'],
+    packageMains: ['browser', 'web', 'browserify', 'main', 'style']
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loaders: ['eslint'],
+        exclude: /node_modules/
+      }
     ],
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'spec.js',
-        publicPath: '/build/'
-    },
-    resolve: {
-        extensions: ['', '.scss', '.js', '.json'],
-        packageMains: ['browser', 'web', 'browserify', 'main', 'style']
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: [/(node_modules)/, /react-css-themr/]
-            }, {
-                test: /\.(scss|css)$/,
-                loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap')
-            }
-        ]
-    },
-    postcss: [autoprefixer],
-    plugins: [
-        new ExtractTextPlugin('spec.css', { allChunks: true }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development'),
-            VERSION: JSON.stringify(pkg.version)
-        })
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel',
+        exclude: [/(node_modules)/]
+      },
+      {
+        test: /\.(scss|css)$/,
+        loader: 'style!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass'
+      }
     ]
+  },
+  postcss: [autoprefixer],
+  eslint: {
+    configFile: paths.project('.eslintrc')
+  },
+  plugins: [
+    new ExtractTextPlugin('spec.css', {allChunks: true}),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+      VERSION: JSON.stringify(pkg.version)
+    })
+  ]
 };
